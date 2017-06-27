2cf8d963493b3d4a1ea4791d2f92828e49a6e2a4
kitze <kristijan.mkd@gmail.com>
2017-06-27 10:11:03 +0200

    🛠 add silent updates with notification, update to 0.1.6

--- public/electron.js
+++ public/electron.js
@@ -1,12 +1,12 @@
 const electron = require('electron');
 const app = electron.app;
-const dialog = electron.dialog;
-
 const BrowserWindow = electron.BrowserWindow;
 
+const platform = require('os').platform;
 const path = require('path');
 const url = require('url');
 const isDev = require('electron-is-dev');
+const notifier = require('node-notifier');
 
 let mainWindow;
 
@@ -20,61 +20,44 @@ function createWindow() {
       : `file://${path.join(__dirname, '../build/index.html')}`
   );
   mainWindow.on('closed', () => (mainWindow = null));
-
   initAutoUpdate();
 }
 
 function initAutoUpdate() {
+  if (isDev) {
+    return;
+  }
 
-  let updater = {}
+  const platform = platform();
+  if (platform === 'linux') {
+    return;
+  }
+
   autoUpdater.checkForUpdates();
+  autoUpdater.signals.updateDownloaded(showUpdateNotification);
+}
 
-  autoUpdater.on('error', (event, error) => {
-    dialog.showErrorBox(
-      'Error: ',
-      error == null ? 'unknown' : (error.stack || error).toString()
-    );
-  });
+function showUpdateNotification(it) {
+  it = it || {};
+  const restartNowAction = 'Restart now';
 
-  autoUpdater.on('update-available', () => {
-    dialog.showMessageBox(
-      {
-        type: 'info',
-        title: 'Found Updates',
-        message: 'Found updates, do you want update now?',
-        buttons: ['Sure', 'No']
-      },
-      buttonIndex => {
-        if (buttonIndex === 0) {
-          autoUpdater.downloadUpdate();
-        } else {
-          updater.enabled = true;
-          updater = null;
-        }
+  const versionLabel = it.label ? `Version ${it.version}` : 'The latest version';
+
+  notifier.notify(
+    {
+      title: 'A new update is ready to install.',
+      message: `${versionLabel} has been downloaded and will be automatically installed after restart.`,
+      closeLabel: 'Okay',
+      actions: restartNowAction
+    },
+    function(err, response, metadata) {
+      if (err) throw err;
+      if (metadata.activationValue !== restartNowAction) {
+        return;
       }
-    );
-  });
-
-  autoUpdater.on('update-not-available', () => {
-    dialog.showMessageBox({
-      title: 'No Updates',
-      message: 'Current version is up-to-date.'
-    });
-    updater.enabled = true;
-    updater = null;
-  });
-
-  autoUpdater.on('update-downloaded', () => {
-    dialog.showMessageBox(
-      {
-        title: 'Install Updates',
-        message: 'Updates downloaded, application will be quit for update...'
-      },
-      () => {
-        autoUpdater.quitAndInstall();
-      }
-    );
-  });
+      autoUpdater.quitAndInstall();
+    }
+  );
 }
 
 app.on('ready', createWindow);
