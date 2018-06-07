091eaa1059c08d16bba9039c8f8dafa2d05b9b7d
kitze <kristijan.mkd@gmail.com>
2018-06-07 12:12:02 +0200

    release 0.2.5

--- public/electron.js
+++ public/electron.js
@@ -1,36 +1,36 @@
-const electron = require('electron');
+const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path');
-const isDev = require('electron-is-dev');
-const initAutoUpdate = require('update-electron-app');
+const path = require("path");
+const isDev = require("electron-is-dev");
 
 let mainWindow;
 
-initAutoUpdate({
-  repo: 'kitze/react-electron-example'
+require("update-electron-app")({
+  repo: "kitze/react-electron-example",
+  updateInterval: "1 hour"
 });
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow = new BrowserWindow({ width: 900, height: 680 });
   mainWindow.loadURL(
     isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
   );
-  mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.on("closed", () => (mainWindow = null));
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (mainWindow === null) {
     createWindow();
   }
