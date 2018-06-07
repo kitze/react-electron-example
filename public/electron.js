4f588bc87cf6f4d9e9e623172497646f2d487146
kitze <kristijan.mkd@gmail.com>
2018-06-07 11:35:51 +0200

    switch update mechanism

--- public/electron.js
+++ public/electron.js
@@ -3,13 +3,14 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
-const url = require('url');
 const isDev = require('electron-is-dev');
-const notifier = require('node-notifier');
+const initAutoUpdate = require('update-electron-app');
 
 let mainWindow;
 
-const autoUpdater = require('electron-updater').autoUpdater;
+initAutoUpdate({
+  repo: 'kitze/react-electron-example'
+});
 
 function createWindow() {
   mainWindow = new BrowserWindow({width: 900, height: 680});
@@ -19,43 +20,6 @@ function createWindow() {
       : `file://${path.join(__dirname, '../build/index.html')}`
   );
   mainWindow.on('closed', () => (mainWindow = null));
-  initAutoUpdate();
-}
-
-function initAutoUpdate() {
-  if (isDev) {
-    return;
-  }
-
-  if (process.platform === 'linux') {
-    return;
-  }
-
-  autoUpdater.checkForUpdates();
-  autoUpdater.signals.updateDownloaded(showUpdateNotification);
-}
-
-function showUpdateNotification(it) {
-  it = it || {};
-  const restartNowAction = 'Restart now';
-
-  const versionLabel = it.label ? `Version ${it.version}` : 'The latest version';
-
-  notifier.notify(
-    {
-      title: 'A new update is ready to install.',
-      message: `${versionLabel} has been downloaded and will be automatically installed after restart.`,
-      closeLabel: 'Okay',
-      actions: restartNowAction
-    },
-    function(err, response, metadata) {
-      if (err) throw err;
-      if (metadata.activationValue !== restartNowAction) {
-        return;
-      }
-      autoUpdater.quitAndInstall();
-    }
-  );
 }
 
 app.on('ready', createWindow);
